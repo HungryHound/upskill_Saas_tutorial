8124a0ca2e3b5227c7ab3cb4722eea449759890b
HungryHound <igor.liubarsky@gmail.com>
2017-09-26 11:18:01 +0000

    Added association for plans and users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'pages#home'
   get 'about', to: 'pages#about'
   resources :contacts, only: :create
   get 'contact-us', to: 'contacts#new', as: 'new_contact'
+  resources :cryptocurrencies
+  resources :calculator
 end
